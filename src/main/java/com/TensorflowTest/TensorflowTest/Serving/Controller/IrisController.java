82584a992c26d7671c2253fc5788bc6f56089c76
Peter Rydberg <hmp.rydberg@gmail.com>
2018-08-02 08:55:52 +0200

    Added form and controller update for ONNX

--- src/main/java/com/TensorflowTest/TensorflowTest/Serving/Controller/IrisController.java
+++ src/main/java/com/TensorflowTest/TensorflowTest/Serving/Controller/IrisController.java
@@ -4,8 +4,11 @@ import com.TensorflowTest.TensorflowTest.Serving.Iris;
 import com.TensorflowTest.TensorflowTest.Serving.Service.IrisClassifierService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @RestController
@@ -15,8 +18,17 @@ public class IrisController {
     IrisClassifierService irisClassifierService;
 
     @GetMapping(value = "/iris/classify/class")
-    public Iris.IrisType classify(Iris iris) {
-        return irisClassifierService.classify(iris);
+    public Iris.IrisType classify(Iris iris, @Valid @RequestParam String modeltype) {
+
+        if (modeltype.equals("tensorflow")){
+            return irisClassifierService.classify(iris);
+        }
+        else if (modeltype.equals("onnx")){
+            //System.out.println("ONNX here");
+
+            return irisClassifierService.classify(iris);
+        }
+        return null;
     }
 
     @GetMapping(value = "/iris/classify/probabilities")
